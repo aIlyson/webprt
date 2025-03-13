65aabb7e8ee220ec46295ab2d897cf1b6b88b793
aIlyson <alyssonmichel20@gmail.com>
2025-03-13 15:54:57 -0300

    init

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+const Home: React.FC = (): JSX.Element => {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-gray-900 text-white">
+      <div className="text-center">
+        <img src="/images/kyubey.gif" alt="Construção" className="mb-4" />
+        <h1 className="text-4xl font-bold">init...</h1>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
