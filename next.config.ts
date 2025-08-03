57b26c674a64cb4d0ee7e6235061f9e659f699bc
aIlyson <alyssonmichel20@gmail.com>
2025-08-03 19:40:52 -0300

    bugfix: fix redirect issue?

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/:path+',
+        destination: '/',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
